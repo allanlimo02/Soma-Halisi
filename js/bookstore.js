e1e2a083abab9f17e4491e083426e8d0f791a8b0
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-04 10:57:01 +0300

    add js CDN and also add changes on js file

--- js/bookstore.js
+++ js/bookstore.js
@@ -1,60 +1,44 @@
-$(document).ready(function(event){
+$(document).ready(function(){
     $('#btnmain').click(function(){
        $(".formcont").show() 
     })
-});
+
 
     
-//     var Nairobi={
-//         shop1:'Jabavu Book shop',
-//         shop2:'Mboya book center',
-//         shop3:'Kenvy Book store'
-//     }
-//     var mombasa={
-//         shop1:' Bakari shop',
-//         shop2:'Taqviw book center',
-//         shop3:'Buhari Book store'
-//     }
-//     var nakuru={
-//         shop1:'Kamau Book shop',
-//         shop2:'Njugunas book center',
-//         shop3:'Nakuru Book store'
-//     }
-//     var eldoret={
-//         shop1:'Ndalat Book shop',
-//         shop2:'Moiben book center',
-//         shop3:'Chepbilat Book store'
-//     }
-//     var kericho={
-//         shop1:'Koiywa Book shop',
-//         shop2:'Kipchimchim book center',
-//         shop3:'Ainamoi Book store'
-//     }
-//     var nyeri={
-//         shop1:'Mukurwe-ini Book shop',
-//         shop2:'Kagumo book center',
-//         shop3:'Kagochi Book store'
-//     }
-//     //var uname= document.getElementById('uname').value
-//     var uName=$("input #uname").val();
-//     var publisher=$('#publisher option:selected').val();
-//     var book=$('#book option:selected').val();
-//     var location=parseInt($('#location option:selected').val())
-
-// //    function returnDetails(){
-// //        if(value==0){
-// //            return Nairobi;
-// //        }
-// //    }
-
-//     $('#checkvend').click(function(){
-//         $('#outputName').text('Dear '+uName);
-//         $('#outputtown').text('You have selected '+maddress+' town');
-//         $('#outputgenre').text('You have selected '+book);
-//     })
+    var Nairobi={
+        shop1:'Jabavu Book shop',
+        shop2:'Mboya book center',
+        shop3:'Kenvy Book store'
+    }
+    var mombasa={
+        shop1:' Bakari shop',
+        shop2:'Taqviw book center',
+        shop3:'Buhari Book store'
+    }
+    var nakuru={
+        shop1:'Kamau Book shop',
+        shop2:'Njugunas book center',
+        shop3:'Nakuru Book store'
+    }
+    var eldoret={
+        shop1:'Ndalat Book shop',
+        shop2:'Moiben book center',
+        shop3:'Chepbilat Book store'
+    }
+    var kericho={
+        shop1:'Koiywa Book shop',
+        shop2:'Kipchimchim book center',
+        shop3:'Ainamoi Book store'
+    }
+    var nyeri={
+        shop1:'Mukurwe-ini Book shop',
+        shop2:'Kagumo book center',
+        shop3:'Kagochi Book store'
+    }
+ 
 
 
-// })
+
 function CheckBooks(name, publisher,books,location){
     this.name=name;
     this.publisher=publisher;
@@ -63,17 +47,17 @@ function CheckBooks(name, publisher,books,location){
 }
 CheckBooks.prototype.glocation=function(){
     if(this.location==0){
-        return "Nairobi";
+        return Nairobi;
     }else if(this.location==1){
-        return "Mombasa"
+        return Mombasa
     }else if(this.location==2){
-        return "Nakuru"
+        return Nakuru
     }else if(this.location==3){
-        return "Kericho"
+        return Kericho
     }else if(this.location==4){
-        return "Eldoret"
+        return Eldoret
     }else if(this.location==5){
-        return "Nyeri"
+        return Nyeri
     }
 
 }
@@ -106,9 +90,9 @@ $(document).ready(function(){
     $("#checkvend").click(function(event){
         event.preventDefault();
         var uName=document.getElementById("uname").value;
-        var publisher1=$("#publisher option :selected").val();
-        var book2=parseInt($("#book option:selected").val());
-        var location3=parseInt($("#location option:selected").val());
+        var publisher1=document.getElementById("publisher").value;
+        var book2=document.getElementById("book").value;
+        var location3=document.getElementById("location").value;
         var newDetails=new CheckBooks(publisher1,book2,location3);
         var bookss= newDetails.gbooks();
         var loccationPlace= newDetails.glocation();
@@ -117,11 +101,12 @@ $(document).ready(function(){
 
 
         $("#outputName").text('Dear '+uName);
-        $("#outputpublisher").text('You have selected '+publishers);
-        $("#outputtown").text('You have selected '+loccationPlace+ ' town');
-        $("#outputgenre").text('You have selected '+bookss);
+        $("#outputpublisher").text('You have selected '+publisher1);
+        $("#outputtown").text('You have selected '+location3+ "  town");
+        $("#outputgenre").text('You have selected '+book2);
         
 
     });
 
+});
 });
