acaa1ac5bb69f4d9173b77a6e32cd2e98efc02d1
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-04 22:14:49 +0300

    error added to form

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@ function validate(){
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
     var message=document.getElementById("message").value;
-    if(name!=="" && email!=="" && message!==""){
-        alert("hi" + " " +name +" " +"welcome to Soma Halisi. W'll get back to you ASAP!");
-    }
-    else if(name!=="" || email!=="" || message!==""){
-        alert("please enter your details");  
-    }
+    // if(name!=="" && email!=="" && message!==""){
+    //     alert("hi" + " " +name +" " +"welcome to Soma Halisi. W'll get back to you ASAP!");
+    // }
+    // else if(name!=="" || email!=="" || message!==""){
+    //     alert("please enter your details");  
+    // }
+    if(name==""){
+        $("#error1").show()
+        return false
+    }else
+    $("#error1").hide();
+    if(email==""){
+        $("#error2").show();
+        return false
+    }else
+        $("#error2").hide();
+    if(message==""){
+        $("#error3").show();
+        return false
+        
+    }else
+    alert("hi" +" " +name +" " +"welcome to Soma Halisi and w'll get back to you ASAP!")
+
 }
+
