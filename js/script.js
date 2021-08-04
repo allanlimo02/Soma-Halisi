0df7f2ed934cc4fc957dce2a5fa5ab3dac09ae88
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-04 12:27:03 +0300

    final updated contact

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@ function validate(){
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
     var message=document.getElementById("message").value;
-    if(name==="" && email==="" && message===""){
-        alert("please provide your details");
-         return false;
+    if(name!=="" && email!=="" && message!==""){
+        alert("hi" + " " +name +" " +"welcome to Soma Halisi");
     }
-    else if(name==="" || email==="" || message===""){
-        alert("please provide valid details")
+    else if(name!=="" || email!=="" || message!==""){
+        alert("please enter your details");  
     }
 }
