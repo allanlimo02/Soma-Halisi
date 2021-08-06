1a924a9608d885487a23e3952ad9715359977a72
allanlimo02 <allan.limo@student.moringaschool.com>
2021-08-06 11:24:59 +0300

    Unsaved changes

--- js/bookstore.js
+++ js/bookstore.js
@@ -149,7 +149,7 @@ $(document).ready(function(){
             $("#outputtown").text('You have selected '+location3+ "  town as your current location");
             $("#outputgenre").text('You have also selected '+book2);
             $("#listofshops").text('We have the following shops in '+location3);
-            $("#shop1").text("1: "+newLoc.shop1);
+                $("#shop1").text("1: "+newLoc.shop1);
             $("#shop2").text("2: "+newLoc.shop2);
             $("#shop3").text("3: "+newLoc.shop3);
             event.preventDefault();
